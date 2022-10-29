d25129441fee2847f469fbe3d3f4bf2a9c748ecd
Anurag Suraj Bhardwaj <105678749+BhardwajAnurag@users.noreply.github.com>
2022-10-29 20:08:13 +0000

    Initial commit

---
+++ day8_100days.py
@@ -0,0 +1,17 @@
+print("Welcome to custom affirmations generator!")
+print("-----------------------------------------")
+name = input("What is your name? \n")
+if name == "Mark" or name == "mark":
+  print("Welcome Mark! \nPlease answer the following questions")
+  day = input("What's the current day of week?\n")
+  if day == "Monday" or day == "monday":
+    print("Amazing you are here on first day of the week")
+    food = input("What's your favourite thing to eat? \n")
+    if food == "Chocolate" or food == "chocolate":
+      print("Amazing\nYou are the most chocliest person I have met", name, "\nI hope you have the chocoful day")
+    else:
+      print(f"Sorry we don't like your taste in {food}")
+  else:
+    print("Well come back on Monday")
+else:
+  print(f"I just don't like you {name}, Goodbye!")
